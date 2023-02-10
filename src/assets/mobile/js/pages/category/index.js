1a1945d1368fdc6380295adb48a87723ea56e013
dansestiner <d.oliveira@sestini.com.br>
2023-02-10 10:44:04 -0300

    FEAT - Qualidade de fotos em Mobile

--- src/assets/mobile/js/pages/category/index.js
+++ src/assets/mobile/js/pages/category/index.js
@@ -6,6 +6,45 @@ const init = () => {
   Common.init();
   FilterButton.init();
   DescriptionAccordion.init();
+
+  $(document).ready(function() {
+    $('body').addClass('loadBestShelf');
+    function bestResolutionImage(){    
+        $('.x-shelf__item .x-shelf__placeloader').each(function(){
+          let imageLinkFront = $(this).find('.x-shelf__img-front').attr('src').replace('157-189', '500-500');
+          $(this).find('.x-shelf__img-front').attr('src', imageLinkFront);
+    
+    
+          if( $(window).width() > 1025 ){
+              let imageLinkBack = $(this).find('.x-shelf__img-back').attr('src').replace('157-189', '500-500');
+              $(this).find('.x-shelf__img-back').attr('src', imageLinkBack);
+          }        
+        });
+    }
+    $('.cf-pagination').on('click', function(){
+      var qtd = 0;
+      var loadBestImage = setInterval(function () {
+          bestResolutionImage()
+          qtd++;
+          if (qtd > 4) clearInterval(loadBestImage);
+      }, 1500);
+    })
+    $('.multi-search-checkbox').on('click', function(){
+        var qtd = 0;
+        var loadBestImage = setInterval(function () {
+            bestResolutionImage()
+            qtd++;
+            if (qtd > 4) clearInterval(loadBestImage);
+        }, 1500);
+    })
+    setInterval(function () {
+      if($('.x-shelf__img-front').length > 0){
+        bestResolutionImage();
+        return false;
+      }
+    }, 20);  
+});
+
 };
 
 export default {
--- src/assets/mobile/js/pages/general/index.js
+++ src/assets/mobile/js/pages/general/index.js
@@ -10,6 +10,38 @@ const init = () => {
   GeneralFooter.init();
   popupLogin.init();
   imageSearch.init();
+
+  $(document).ready(function() {
+    $('body').addClass('loadBestShelf');
+    function bestResolutionImage(){   
+        $('.x-shelf__img-container .x-shelf__img-front').each(function(){
+          let imageLinkFront = $(this).attr('src').replace('157-189', '500-500');
+          $(this).attr('src', imageLinkFront);
+    
+          if( $(window).width() > 1025 ){
+              let imageLinkBack = $(this).find('.x-shelf__img-back').attr('src').replace('157-189', '500-500');
+              $(this).find('.x-shelf__img-back').attr('src', imageLinkBack);
+          }
+        });
+
+        $('.x-shelf__img-container .x-shelf__img-front').each(function(){
+          let imageLinkFront = $(this).attr('src').replace('240-289', '500-500');
+          $(this).attr('src', imageLinkFront);
+    
+          if( $(window).width() > 1025 ){
+              let imageLinkBack = $(this).find('.x-shelf__img-back').attr('src').replace('157-189', '500-500');
+              $(this).find('.x-shelf__img-back').attr('src', imageLinkBack);
+          }
+        });
+    }
+    
+    setInterval(function () {
+      if($('.x-shelf__img-front').length > 0){
+        bestResolutionImage();
+        return false;
+      }
+    }, 20);
+  });
 };
 
 export default {
